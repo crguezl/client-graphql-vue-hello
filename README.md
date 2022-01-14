a370fdde67a3372c478573a31658950a23e19ef7
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-01-14 08:00:38 +0000

    README.md src/App.vue

--- README.md
+++ README.md
@@ -33,3 +33,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## References
+
+* [Location.reload()](https://developer.mozilla.org/en-US/docs/Web/API/Location/reload)
--- src/App.vue
+++ src/App.vue
@@ -1,163 +1,199 @@
 <template>
   <div id="app">
-  <h1>Agenda</h1>
-  <table border='1' width='100%' style='border-collapse: collapse;'>
-   <tr>
-     <th>ID</th>
-     <th>First Name</th>
-     <th>Last Name</th>
-     <th>Email</th>
-     <th>Actions</th>
-   </tr>
+    <h1>Agenda</h1>
+    <table border="1" width="100%" style="border-collapse: collapse">
+      <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Actions</th>
+      </tr>
 
-   <tr v-for='contact in contacts' :key="contact.id">
-     <td>{{  contact.id }}</td>
-     <td>{{ contact.firstName }}</td>
-     <td>{{ contact.lastName }}</td>
-     <td>{{ contact.email }}</td>
-     <td>
-      <input type="button" @click="selectContact(contact)" value="Select" class="btn btn-primary">
-      <input type="button" @click="deleteContact(contact.id)" value="Delete" class="btn btn-cancel">
-     </td> 
-   </tr>
- </table>
+      <tr v-for="contact in contacts" :key="contact.id">
+        <td>{{ contact.id }}</td>
+        <td>{{ contact.firstName }}</td>
+        <td>{{ contact.lastName }}</td>
+        <td>{{ contact.email }}</td>
+        <td>
+          <input
+            type="button"
+            @click="selectContact(contact)"
+            value="Select"
+            class="btn btn-primary"
+          />
+          <input
+            type="button"
+            @click="deleteContact(contact.id)"
+            value="Delete"
+            class="btn btn-cancel"
+          />
+        </td>
+      </tr>
+    </table>
 
- <br/> 
-      <div v-if="id">
-        <h2>Update Contact</h2>
-      </div>
-      <div v-else>
-        <h2>Create new Contact</h2>
-      </div>
+    <br />
+    <div v-if="id">
+      <h2>Update Contact</h2>
+    </div>
+    <div v-else>
+      <h2>Create new Contact</h2>
+    </div>
 
     <form>
       <label>First Name: </label>
-      <input type="text" name="firstName" v-model="firstName">
-      <br/>
+      <input type="text" name="firstName" v-model="firstName" />
+      <br />
 
       <label>Last Name: </label>
-      <input type="text" name="lastName" v-model="lastName">
-      <br/>
+      <input type="text" name="lastName" v-model="lastName" />
+      <br />
 
-      <label>Email:     </label>
-      <input type="email" name="email" v-model="email">
-      <br/>
-      
-      <input 
-        v-if="id" 
+      <label>Email: </label>
+      <input type="email" name="email" v-model="email" />
+      <br />
+
+      <input
+        v-if="id"
         type="button"
         class="btn btn-primary"
-        @click="updateContact(id, firstName, lastName, email)" 
-        value="Update Contact">
-      <input 
-      v-else 
-        type="button" 
+        @click="updateContact(id, firstName, lastName, email)"
+        value="Update Contact"
+      />
+      <input
+        v-else
+        type="button"
         class="btn btn-primary"
-        @click="createContact(firstName, lastName, email)" value="Add new Contact">
-       
-      <input 
-        type="button" class="btn btn-cancel"
-        @click="clearForm()" 
-        value="Clear Form">  
+        @click="createContact(firstName, lastName, email)"
+        value="Add new Contact"
+      />
+
+      <input
+        type="button"
+        class="btn btn-cancel"
+        @click="clearForm()"
+        value="Clear Form"
+      />
     </form>
-</div>
+  </div>
 </template>
 
 <script>
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 
 export default {
-  name: 'app',
-  data(){
+  name: "app",
+  data() {
     return {
       id: null,
-      firstName: '',
-      lastName: '',
-      email: ''}
+      firstName: "",
+      lastName: "",
+      email: "",
+    };
   },
   apollo: {
-    contacts: gql`query {
-      contacts {
-        id,
-        firstName,
-        lastName,
-        email
+    contacts: gql`
+      query {
+        contacts {
+          id
+          firstName
+          lastName
+          email
+        }
       }
-    }`,
-
+    `,
   },
   methods: {
-    createContact(firstName, lastName, email){
-      console.log(`Create contact: ${email}`)
+    createContact(firstName, lastName, email) {
+      console.log(`Create contact: ${email}`);
       this.$apollo.mutate({
-          mutation: gql`mutation createContact($firstName: String!, $lastName: String!, $email: String!){
-            createContact(firstName: $firstName, lastName: $lastName, email: $email) {
-              id,
-              firstName,
-              lastName,
-              email}
-          }`,
-          variables:{
-            firstName: firstName,
-            lastName: lastName,
-            email: email
+        mutation: gql`
+          mutation createContact(
+            $firstName: String!
+            $lastName: String!
+            $email: String!
+          ) {
+            createContact(
+              firstName: $firstName
+              lastName: $lastName
+              email: $email
+            ) {
+              id
+              firstName
+              lastName
+              email
+            }
           }
-        }
-      )
+        `,
+        variables: {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        },
+      });
       location.reload();
     },
-    updateContact(id, firstName, lastName, email){
-      console.log(`Update contact: # ${id}`)
+    updateContact(id, firstName, lastName, email) {
+      // eslint-disable-next-line no-console
+      console.log(`Update contact: # ${id}`);
       this.$apollo.mutate({
-          mutation: gql`mutation updateContact(
-            $id: ID!, 
-            $firstName: String, 
-            $lastName: String!, 
-            $email: String!) {
-            updateContact(id: $id, firstName: $firstName, lastName: $lastName, email: $email) {
-              id,
-              firstName,
-              lastName,
-              email}
+        mutation: gql`
+          mutation updateContact(
+            $id: ID!
+            $firstName: String
+            $lastName: String!
+            $email: String!
+          ) {
+            updateContact(
+              id: $id
+              firstName: $firstName
+              lastName: $lastName
+              email: $email
+            ) {
+              id
+              firstName
+              lastName
+              email
+            }
           }
-          `,
-          variables:{
-            id: id,
-            firstName: firstName,
-            lastName: lastName,
-            email: email
-          }
-        }
-      )
-      location.reload();
+        `,
+        variables: {
+          id: id,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        },
+      });
+      location.reload(); // refresh the page
     },
-    deleteContact(id){
-      console.log(`Delete contact: # ${id}`)
+    deleteContact(id) {
+      console.log(`Delete contact: # ${id}`);
       this.$apollo.mutate({
-          mutation: gql`mutation deleteContact($id: ID!){
+        mutation: gql`
+          mutation deleteContact($id: ID!) {
             deleteContact(id: $id)
-          }`,
-          variables:{
-            id: id,
           }
-        }
-      )
+        `,
+        variables: {
+          id: id,
+        },
+      });
       location.reload();
     },
-    selectContact(contact){
+    selectContact(contact) {
       this.id = contact.id;
       this.firstName = contact.firstName;
       this.lastName = contact.lastName;
       this.email = contact.email;
     },
-    clearForm(){
+    clearForm() {
       this.id = null;
-      this.firstName = '';
-      this.lastName = '';
-      this.email = '';
-    }    
-  }
-}
+      this.firstName = "";
+      this.lastName = "";
+      this.email = "";
+    },
+  },
+};
 </script>
 
 <style scoped src="./App.css">
