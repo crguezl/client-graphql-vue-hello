d9595cdc127557a5aefe6ea734f8f8919fc44dc5
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-01-18 13:36:27 +0000

    comments

--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@ const apolloClient = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 })
 
+console.log(apolloClient)
+
 Vue.use(VueApollo)
 
+// The provider holds the Apollo client instances that can then be used by all the child components
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
 })
@@ -16,6 +19,6 @@ const apolloProvider = new VueApollo({
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App), // See https://crguezl.github.io/learning-vue-geting-started-guide/#an-introduction-to-render-functions
   apolloProvider,
 }).$mount('#app')
